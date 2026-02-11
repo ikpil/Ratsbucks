43cb9dc8021eb3247595dda46c1370b0556f1da3
ikpil <ikpil@naver.com>
2026-02-12 02:17:29 +0900

    이 파일의 데이터를 토대로 데이터를 구조화 해줘
    요구사항
    - order_product 클래스를 추가해줘
    - order_product 클래스에는 이름, 영어이름, 가격, 이미지 변수가 있고, 메인 카테고리, 서브 카테고리를 멤버 변수로 갖는다
    - #file:order_item_sample_list.dart 파일을 읽고, order_product_list 를 구성해줘
    
    프롬프트 복사, 제한 걸려서 못쓰ㅡㅁ

---
+++ lib/models/order_product.dart
@@ -0,0 +1,22 @@
+import 'order_main_category_id.dart';
+import 'order_sub_category_id.dart';
+
+class OrderProduct {
+  final String name;
+  final String enName;
+  final String price; // Using String to match existing data format ('6,000')
+  final String imageUrl;
+  final OrderMainCategoryId mainCategoryId;
+  final OrderSubCategoryId subCategoryId;
+
+  const OrderProduct({
+    required this.name,
+    required this.enName,
+    required this.price,
+    required this.imageUrl,
+    required this.mainCategoryId,
+    required this.subCategoryId,
+  });
+
+  int get priceInt => int.parse(price.replaceAll(',', ''));
+}
